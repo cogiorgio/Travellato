558b211683ae765cb7f1b0bb2ebc6f8be65170a5
cogiorgio <cocciagiorgio96@hotmail.it>
2020-11-25 13:54:44 +0100

    ciao isabella

---
+++ lib/login.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  Function callback;
+  Login(this.callback);
+  @override
+  @override
+  Widget build(BuildContext context) {
+    // This app is designed only to work vertically, so we limit
+    // orientations to portrait up and down.
+    final double h = MediaQuery.of(context).size.height;
+    final double w = MediaQuery.of(context).size.width;
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Container(
+        height: 0.23 * h,
+      ),
+      Container(
+        alignment: Alignment.center,
+        height: 0.32 * h,
+        padding: EdgeInsets.only(top: 0.0 * h),
+        child: Container(
+            padding: EdgeInsets.all(30),
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+                color: Color(0xFFEEF5FD),
+                borderRadius: BorderRadius.all(Radius.circular(300))),
+            child: ClipRRect(
+                child: Image(image: AssetImage('graphics/COFFEE.png')))),
+      ),
+      Container(
+        height: 0.02 * h,
+      ),
+      Container(
+        height: 0.06 * h,
+        alignment: Alignment.center,
+        child: RaisedButton(
+          onPressed: () {},
+          disabledColor: Color(0xFFEEF5FD),
+          highlightColor: Color(0xFFEEF5FD),
+          splashColor: Color(0xFF548CCC),
+          elevation: 1,
+          disabledElevation: 1.0,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(50))),
+          color: Color(0xFF9DC3EE),
+          child: Container(
+              alignment: Alignment.center,
+              width: 150,
+              child: Text("Sign In", style: TextStyle(color: Colors.white))),
+        ),
+      ),
+      Container(
+        height: 0.02 * h,
+      ),
+      Container(
+        height: 0.06 * h,
+        alignment: Alignment.center,
+        child: RaisedButton(
+            onPressed: () {
+              this.callback();
+            },
+            disabledColor: Color(0xFFEEF5FD),
+            highlightColor: Color(0xFFEEF5FD),
+            splashColor: Color(0xFF548CCC),
+            elevation: 1,
+            disabledElevation: 1.0,
+            shape: RoundedRectangleBorder(
+                side: BorderSide(color: Color(0xFF548CCC), width: 0.6),
+                borderRadius: BorderRadius.all(Radius.circular(50))),
+            color: Color(0xFFEEF5FD),
+            child: Container(
+                alignment: Alignment.center,
+                width: 150,
+                child: Text(
+                  "Log In",
+                  style: TextStyle(color: Color(0xFF548CCC)),
+                ))),
+      ),
+      Container(
+        height: 0.02 * h,
+      ),
+      Container(
+        alignment: Alignment.center,
+        height: 0.03 * h,
+        child: Text(
+          "forgot your password?",
+          style: TextStyle(color: Colors.grey),
+        ),
+      ),
+    ]);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,16 +30,24 @@ class Tutorial extends StatefulWidget {
 }
 
 class _Tutorial extends State<Tutorial> with TickerProviderStateMixin {
+  PageController _myPage2;
+  var selectedPage2;
   PageController _myPage;
   var selectedPage;
   Animation<Color> animation;
   AnimationController controller;
+
+  Animation<double> animation2;
+  AnimationController controller2;
 
   @override
   void initState() {
     super.initState();
     _myPage = PageController(initialPage: 0);
     selectedPage = 0;
+
+    _myPage2 = PageController(initialPage: 0);
+    selectedPage2 = 0;
 
     controller = AnimationController(
         duration: const Duration(milliseconds: 800), vsync: this);
@@ -49,24 +58,42 @@ class _Tutorial extends State<Tutorial> with TickerProviderStateMixin {
               // The state that has changed here is the animation object’s value.
             });
           });
+    controller2 = AnimationController(
+        duration: const Duration(milliseconds: 1800), vsync: this);
+    animation2 = Tween(begin: 0.0, end: 800.0).animate(new CurvedAnimation(
+        parent: controller2,
+        curve: Curves.easeInOutBack,
+        reverseCurve: Curves.easeOut))
+      ..addListener(() {
+        setState(() {
+          // The state that has changed here is the animation object’s value.
+        });
+      });
+  }
+
+  callback() {
+    controller2.forward();
   }
 
   @override
   Widget build(BuildContext context) {
     controller.forward();
+
     final double h = MediaQuery.of(context).size.height;
     final double w = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        height: h,
-        width: w,
-        color: Color(0xFFEEF5FD),
-        padding: EdgeInsets.only(top: 0.08 * h),
+        body: Container(
+      height: h,
+      width: w,
+      color: Color(0xFFEEF5FD),
+      padding: EdgeInsets.only(top: 0.08 * h),
+      child: Transform.translate(
+        offset: Offset(0, animation2.value),
         child: Container(
           decoration: BoxDecoration(
               boxShadow: [
                 BoxShadow(
-                  blurRadius: 10, // soften the shadow
+                  blurRadius: 1, // soften the shadow
                   spreadRadius: 0.001,
                   color: Color.fromRGBO(84, 140, 204, 0.2),
                   offset: Offset(0, -1),
@@ -74,156 +101,181 @@ class _Tutorial extends State<Tutorial> with TickerProviderStateMixin {
               ],
               color: Colors.white,
               borderRadius: BorderRadius.all(Radius.circular(40))),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 0.05 * h,
-              ),
-              RaisedButton(
-                padding: EdgeInsets.all(15),
-                splashColor: Colors.red,
-                disabledElevation: 1.0,
-                shape: CircleBorder(),
-                disabledColor: Color(0xFFEEF5FD),
-                child: Icon(Icons.clear, color: Color(0xFF548CCC)),
-              ),
-              Container(
-                height: 0.01 * h,
-              ),
-              selectedPage == 0
-                  ? Container(
-                      height: 0.1 * h,
-                      alignment: Alignment.center,
-                      child: Text("Welcome", style: TextStyle(fontSize: 40)))
-                  : selectedPage == 1
-                      ? Container(
-                          height: 0.1 * h,
-                          alignment: Alignment.center,
-                          child:
-                              Text("Welcome2", style: TextStyle(fontSize: 40)))
-                      : Container(
-                          height: 0.1 * h,
-                          alignment: Alignment.center,
-                          child:
-                              Text("Welcome3", style: TextStyle(fontSize: 40))),
-              Stack(children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 0.32 * h,
-                  padding: EdgeInsets.only(top: 0.0 * h),
-                  child: Container(
-                      height: 200,
-                      width: 200,
-                      decoration: BoxDecoration(
-                          color: Color(0xFFEEF5FD),
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(300)))),
-                ),
-                SizedBox(
-                  height: 0.37 * h,
-                  width: w,
-                  child: PageView(
-                      onPageChanged: (int i) {
-                        controller.reset();
-                        selectedPage = i;
-
-                        setState() {}
-                        ;
-                        controller.reset();
-                      },
-                      scrollDirection: Axis.horizontal,
-                      controller: _myPage,
-                      children: [Page1(), Page2(), Page3()]),
-                ),
-              ]),
-              selectedPage == 0
-                  ? Container(
+          child: PageView(
+              physics: new NeverScrollableScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              controller: _myPage2,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
                       height: 0.05 * h,
+                    ),
+                    Container(
+                      height: 0.06 * h,
+                      child: RaisedButton(
+                        padding: EdgeInsets.all(10),
+                        splashColor: Colors.red,
+                        disabledElevation: 1.0,
+                        shape: CircleBorder(),
+                        disabledColor: Color(0xFFEEF5FD),
+                        child: Icon(Icons.clear, color: Color(0xFF548CCC)),
+                      ),
+                    ),
+                    Container(
+                      height: 0.01 * h,
+                    ),
+                    selectedPage == 0
+                        ? Container(
+                            height: 0.1 * h,
+                            alignment: Alignment.center,
+                            child:
+                                Text("Welcome", style: TextStyle(fontSize: 40)))
+                        : selectedPage == 1
+                            ? Container(
+                                height: 0.1 * h,
+                                alignment: Alignment.center,
+                                child: Text("Welcome2",
+                                    style: TextStyle(fontSize: 40)))
+                            : Container(
+                                height: 0.1 * h,
+                                alignment: Alignment.center,
+                                child: Text("Welcome3",
+                                    style: TextStyle(fontSize: 40))),
+                    Stack(children: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: Container(
+                            height: 220,
+                            width: 220,
+                            decoration: BoxDecoration(
+                                color: Color(0xFFEEF5FD),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(300)))),
+                      ),
+                      SizedBox(
+                        height: 240,
+                        width: w,
+                        child: PageView(
+                            onPageChanged: (int i) {
+                              controller.reset();
+                              selectedPage = i;
+
+                              setState() {}
+                              ;
+                              controller.reset();
+                            },
+                            scrollDirection: Axis.horizontal,
+                            controller: _myPage,
+                            children: [Page1(), Page2(), Page3()]),
+                      ),
+                    ]),
+                    Container(height: 0.05 * h),
+                    selectedPage == 0
+                        ? Container(
+                            height: 0.05 * h,
+                            alignment: Alignment.center,
+                            child: Text(
+                              "join in the world of travellato in a few\n steps! Scrivi già delle indicazioni qui!",
+                              textAlign: TextAlign.center,
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 14),
+                            ))
+                        : selectedPage == 1
+                            ? Container(
+                                height: 0.05 * h,
+                                alignment: Alignment.center,
+                                child: Text(
+                                  "Share your position and see \nwhere your best friends are!",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 14),
+                                ))
+                            : Container(
+                                height: 0.05 * h,
+                                alignment: Alignment.center,
+                                child: Text(
+                                  "blablablablabl bblabl bla \nblablabl bla!",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 14),
+                                )),
+                    Container(
+                      height: 0.04 * h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            width: 8,
+                            height: 8,
+                            decoration: BoxDecoration(
+                                color: selectedPage == 0
+                                    ? animation.value
+                                    : Color(0xFFC4C4C4),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)))),
+                        SizedBox(width: 5),
+                        Container(
+                            width: 8,
+                            height: 8,
+                            decoration: BoxDecoration(
+                                color: selectedPage == 1
+                                    ? animation.value
+                                    : Color(0xFFC4C4C4),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)))),
+                        SizedBox(width: 5),
+                        Container(
+                            width: 8,
+                            height: 8,
+                            decoration: BoxDecoration(
+                                color: selectedPage == 2
+                                    ? animation.value
+                                    : Color(0xFFC4C4C4),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)))),
+                      ],
+                    ),
+                    Container(
+                      height: 0.06 * h,
+                    ),
+                    Container(
+                      height: 0.06 * h,
                       alignment: Alignment.center,
-                      child: Text(
-                        "join in the world of travellato in a few\n steps! Scrivi già delle indicazioni qui!",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      ))
-                  : selectedPage == 1
-                      ? Container(
-                          height: 0.05 * h,
-                          alignment: Alignment.center,
-                          child: Text(
-                            "Share your position and see \nwhere your best friends are!",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.black, fontSize: 14),
-                          ))
-                      : Container(
-                          height: 0.05 * h,
-                          alignment: Alignment.center,
-                          child: Text(
-                            "blablablablabl bblabl bla \nblablabl bla!",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.black, fontSize: 14),
-                          )),
-              Container(
-                height: 0.04 * h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                      width: 8,
-                      height: 8,
-                      decoration: BoxDecoration(
-                          color: selectedPage == 0
-                              ? animation.value
-                              : Color(0xFFC4C4C4),
-                          borderRadius: BorderRadius.all(Radius.circular(50)))),
-                  SizedBox(width: 5),
-                  Container(
-                      width: 8,
-                      height: 8,
-                      decoration: BoxDecoration(
-                          color: selectedPage == 1
-                              ? animation.value
-                              : Color(0xFFC4C4C4),
-                          borderRadius: BorderRadius.all(Radius.circular(50)))),
-                  SizedBox(width: 5),
-                  Container(
-                      width: 8,
-                      height: 8,
-                      decoration: BoxDecoration(
-                          color: selectedPage == 2
-                              ? animation.value
-                              : Color(0xFFC4C4C4),
-                          borderRadius: BorderRadius.all(Radius.circular(50)))),
-                ],
-              ),
-              Container(
-                height: 0.06 * h,
-              ),
-              Container(
-                height: 0.06 * h,
-                alignment: Alignment.center,
-                child: RaisedButton(
-                  onPressed: () {},
-                  disabledColor: Color(0xFFEEF5FD),
-                  highlightColor: Color(0xFFEEF5FD),
-                  splashColor: Color(0xFF548CCC),
-                  disabledElevation: 1.0,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(50))),
-                  color: Color(0xFFEEF5FD),
-                  child: Container(
-                      alignment: Alignment.center,
-                      width: 150,
-                      child: Text("Skip tutorial",
-                          style: TextStyle(color: Color(0xFF548CCC)))),
+                      child: RaisedButton(
+                        onPressed: () {
+                          _myPage2.animateToPage(1,
+                              duration: Duration(seconds: 1),
+                              curve: Curves.fastOutSlowIn);
+                        },
+                        disabledColor: Color(0xFFEEF5FD),
+                        highlightColor: Color(0xFFEEF5FD),
+                        splashColor: Color(0xFF548CCC),
+                        elevation: 1,
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50))),
+                        color: Color(0xFFEEF5FD),
+                        child: Container(
+                            alignment: Alignment.center,
+                            width: 150,
+                            child: selectedPage != 2
+                                ? Text("Skip tutorial",
+                                    style: TextStyle(color: Color(0xFF548CCC)))
+                                : Text("Continue to login",
+                                    style:
+                                        TextStyle(color: Color(0xFF548CCC)))),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
+                Login(callback)
+              ]),
         ),
       ),
-    );
+    ));
   }
 }
 
@@ -232,20 +284,10 @@ class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     final double h = MediaQuery.of(context).size.height;
     final double w = MediaQuery.of(context).size.width;
-    return Column(children: [
-      Container(
-        height: 0.02 * h,
-      ),
-      Container(
-        height: 0.3 * h,
-        alignment: Alignment.center,
-        child:
-            ClipRRect(child: Image(image: AssetImage('graphics/frame36.png'))),
-      ),
-      Container(
-        height: 0.05 * h,
-      ),
-    ]);
+    return Container(
+      alignment: Alignment.bottomCenter,
+      child: ClipRRect(child: Image(image: AssetImage('graphics/frame36.png'))),
+    );
   }
 }
 
@@ -254,20 +296,11 @@ class Page2 extends StatelessWidget {
   Widget build(BuildContext context) {
     final double h = MediaQuery.of(context).size.height;
     final double w = MediaQuery.of(context).size.width;
-    return Column(children: [
-      Container(
-        height: 0.02 * h,
-      ),
-      Container(
-        height: 0.3 * h,
-        alignment: Alignment.center,
-        child:
-            ClipRRect(child: Image(image: AssetImage('graphics/frame37.png'))),
-      ),
-      Container(
-        height: 0.05 * h,
-      ),
-    ]);
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: EdgeInsets.only(top: 0.01 * h),
+      child: ClipRRect(child: Image(image: AssetImage('graphics/frame37.png'))),
+    );
   }
 }
 
@@ -276,20 +309,10 @@ class Page3 extends StatelessWidget {
   Widget build(BuildContext context) {
     final double h = MediaQuery.of(context).size.height;
     final double w = MediaQuery.of(context).size.width;
-    return Column(children: [
-      Container(
-        height: 0.02 * h,
-      ),
-      Container(
-        height: 0.3 * h,
-        padding: EdgeInsets.only(right: 10),
-        alignment: Alignment.center,
-        child:
-            ClipRRect(child: Image(image: AssetImage('graphics/frame34.png'))),
-      ),
-      Container(
-        height: 0.05 * h,
-      ),
-    ]);
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: EdgeInsets.only(top: 0.01 * h),
+      child: ClipRRect(child: Image(image: AssetImage('graphics/frame34.png'))),
+    );
   }
 }
